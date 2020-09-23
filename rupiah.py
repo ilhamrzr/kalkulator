3dc9fd513915e57b166f26324bdef763e96531c3
IIlham <57558817+ilhamrzr@users.noreply.github.com>
2020-09-23 23:53:07 +0700

    Add files via upload

---
+++ rupiah.py
@@ -0,0 +1,11 @@
+# rupiah.py
+# ---------------
+
+import locale
+
+def rupiah_format(angka, with_prefix=False, desimal=2):
+    locale.setlocale(locale.LC_NUMERIC, 'IND')
+    rupiah = locale.format("%.*f", (desimal, angka), True)
+    if with_prefix:
+        return "Rp. {}".format(rupiah)
+    return rupiah
